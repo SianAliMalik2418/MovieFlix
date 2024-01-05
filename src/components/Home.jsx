a0b622080a6083ba466598e6e2ff73e6f6d0996a
SianAli99 <sianali99@gmail.com>
2024-01-05 21:21:08 +0500

    errors fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 import HeroSection from './HeroSection'
 import CardsContainer from './CardsContainer'
-import Footer from './footer'
 
 
 function Home() {
 
  
-
 
   return (
     <div className='pb-20 '>
