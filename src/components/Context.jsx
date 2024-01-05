28a6f1bc5ec21153df08a433d6eed5be1f49aac5
SianAli99 <sianali99@gmail.com>
2024-01-05 21:27:54 +0500

    error fixed

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -2,13 +2,12 @@ import moviePoster1 from "../assets/batmanTeam.jpg";
 import moviePoster2 from "../assets/Mortal Kombat Movie Poster.jpg";
 import moviePoster3 from "../assets/batman.jpg";
 import moviePoster4 from "../assets/Dog Movie Poster.jpg";
-import moviePoster5 from "../assets/1917 Movie Poster.jpg";
-import moviePoster6 from "../assets/Joker Movie Poster.jpg";
-import moviePoster7 from "../assets/Homelander Movie Poster.png";
-import moviePoster8 from "../assets/SpiderMan Movie Poster.jpg";
-import moviePoster9 from "../assets/aquaman.jpg";
+import moviePoster5 from "../assets/Joker Movie Poster.jpg";
+import moviePoster6 from "../assets/Homelander Movie Poster.png";
+import moviePoster7 from "../assets/SpiderMan Movie Poster.jpg";
+import moviePoster8 from "../assets/aquaman.jpg";
 
-export var heroSectionBigImagePosters = [moviePoster1, moviePoster2, moviePoster3, moviePoster4, moviePoster5, moviePoster6, moviePoster7, moviePoster8,moviePoster9]
+export var heroSectionBigImagePosters = [moviePoster1, moviePoster2, moviePoster3, moviePoster4, moviePoster5, moviePoster6, moviePoster7, moviePoster8]
 
 export var imgPath = "https://image.tmdb.org/t/p/w1280";
 export const API_KEY = "b0032bc987ca01e6a7c02268f471ca6b"
